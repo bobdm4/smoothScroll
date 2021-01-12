aa705806667c7071a9f3cb742a0d2a8387bfaf8a
bobdm4 <asdf514asdf514@gmail.com>
2021-01-12 13:32:06 +0200

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import Navbar from "./components/Navbar";
-import Home from "./pages";
-import SinginPage from "./pages/singin";
+import Navbar from './components/Navbar'
+import Home from './pages'
+import SinginPage from './pages/singin'
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/singin" component={SinginPage} exact />
-      </Switch>
-    </Router>
-  );
+	return (
+		<Router>
+			<Navbar />
+			<Switch>
+				<Route path='/' component={Home} exact />
+				<Route path='/singin' component={SinginPage} exact />
+			</Switch>
+		</Router>
+	)
 }
 
-export default App;
+export default App
--- src/pages/singin.js
+++ src/pages/singin.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ScrollToTop from "../components/ScrollToTop";
-import SingIn from "../components/Singin";
+import React from 'react'
+import ScrollToTop from '../components/ScrollToTop'
+import SingIn from '../components/Singin'
 
 const SinginPage = () => {
-  return (
-    <div>
-      <ScrollToTop />
-      <SingIn />
-    </div>
-  );
-};
+	return (
+		<div>
+			<ScrollToTop />
+			<SingIn />
+		</div>
+	)
+}
 
-export default SinginPage;
+export default SinginPage
